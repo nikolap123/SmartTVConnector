a5b5b34dcbfe1dddcfa45ce025135f98bf7cda8e
josif-popos <joskekostic@gmail.com>
2021-04-19 09:49:07 +0200

    sqlite database started

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/cosmtrek/air v1.26.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	github.com/pelletier/go-toml v1.9.0 // indirect
-	golang.org/x/sys v0.0.0-20210414055047-fe65e336abe0 // indirect
+	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
 )
--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-    "os"
-	"io/ioutil"
+	"os"
+	"time"
 )
 
 
@@ -18,6 +20,10 @@ func main() {
 	http.HandleFunc("/get-applications",HandleGetApplications)
 
 	http.HandleFunc("/run-command", HandleRunCommand)
+
+	http.HandleFunc("/get-devices-db", HanndleGetDevicesDB)
+
+	http.HandleFunc("/insert-devices-db", HanndleInsertDevicesDB)
 
 	log.Fatal(http.ListenAndServe(":8081", nil))
 
@@ -77,4 +83,55 @@ func HandleGetDevices(w http.ResponseWriter, r *http.Request) {
 
 func HandleGetApplications(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Test"))
+}
+
+func HanndleGetDevicesDB(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "./database_lite.db")
+	checkErr(err)
+
+	// query
+	rows, err := db.Query("SELECT * FROM devices")
+	checkErr(err)
+
+	var Id int
+	var Name string
+	var IpAddress string
+	var Year time.Time
+	var Type string
+	var Applications [] Application
+
+	for rows.Next() {
+		err = rows.Scan(&Id, &Name, &IpAddress, &Year, &Type, $Applications)
+		checkErr(err)
+		fmt.Println(Id)
+		fmt.Println(Name)
+		fmt.Println(IpAddress)
+		fmt.Println(Year)
+		fmt.Println(Type)
+	}
+
+	rows.Close()
+}
+
+func HanndleInsertDevicesDB(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("sqlite3", "./database_lite.db")
+	checkErr(err)
+
+	// insert
+	stmt, err := db.Prepare("INSERT INTO devices(name, ipAddress, year, type) values(?,?,?,?)")
+	checkErr(err)
+
+	res, err := stmt.Exec("STB BOX", "192.168.0.58", "2019", "Android")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
